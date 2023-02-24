45e392423b57a3a50d477db9439c3e5e86fe1942
Khadicha <246abdurashidova123@gmail.com>
2023-02-24 16:59:00 +0500

    responsive finished'

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use script";
+
+let openButton = document.querySelector(".button");
+
+let openMenubtn = function () {
+  openButton.style.display = "block";
+};
+
+openButton.addEventListener("click", openMenubtn);
+
+let closeMenubtn = function () {
+  openButton.style.display = "none";
+};
+
+closeBtn.addEventListener("click", closeMenubtn);
