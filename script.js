5766447299232ec9257b0297112335f35cf5fc35
Khadicha <246abdurashidova123@gmail.com>
2023-02-26 10:45:47 +0500

    buttons added1

--- script.js
+++ script.js
@@ -12,31 +12,3 @@ let closeButton = function () {
   openBtn.style.display = "none";
 };
 closeBtn.addEventListener("click", closeButton);
-
-// let openMenubtn = function () {
-//   mainMore.style.display = "block";
-// };
-
-// openBtn.addEventListener("click", openMenubtn);
-
-// let closeMenubtn = function () {
-//   mainMore.style.display = "none";
-// };
-
-// closeBtn.addEventListener("click", closeMenubtn);
-
-// let toogle = false;
-// let toogleFunc = function () {
-//   if (toogle) {
-//     toogle = false;
-//   } else {
-//     toogle = true;
-//   }
-// };
-
-// let toogleBtnFunc = function () {
-//   toogleFunc();
-//   toogleBtn.style.display = toogle ? "olive" : "red";
-// };
-
-// toogleBtn.addEventListener("click", toogleBtnFunc);
